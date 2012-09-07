f485e10a6c34e2ab37aa6addd49fa0a637564343
rcano <rcano@netlabs.com.uy>
2012-09-07 17:09:11 -0300

    Use new logging system and fixed bugs related to lifecycle of underlying endpoint

--- core/src/main/scala/uy/com/netlabs/esb/endpoint/PollingFeatures.scala
+++ core/src/main/scala/uy/com/netlabs/esb/endpoint/PollingFeatures.scala
@@ -8,52 +8,55 @@ import language._
 
 object PollingFeatures {
 
-  class PollAskableEndpoint[A <: Askable, P](f: Flow, endpoint: EndpointFactory[A], every: Duration, message: Message[P])(implicit ev: TypeSupportedByTransport[A#SupportedTypes, P]) extends endpoint.base.BaseSource {
+  class PollAskableEndpoint[A <: Askable, P](f: Flow, endpoint: EndpointFactory[A], initialDelay: Duration, every: Duration, message: Message[P])(implicit ev: TypeSupportedByTransport[A#SupportedTypes, P]) extends endpoint.base.BaseSource {
     lazy val dest = endpoint(f)
     type Payload = A#Response
 
     var scheduledAction: akka.actor.Cancellable = _
     def start() {
-      scheduledAction = flow.schedule(every, every) {
-        println(s"Flow ${flow.name} polling")
+      dest.start()
+      scheduledAction = flow.schedule(initialDelay, every) {
+        flow.log.debug(s"Flow ${flow.name} polling")
         implicit val ec = flow.workerActorsExecutionContext
         dest.ask(message)(ev.asInstanceOf[TypeSupportedByTransport[dest.SupportedTypes, P]]) onComplete {
           case Success(response)  => messageArrived(response.asInstanceOf[Message[Payload]])
-          case Failure(err) => println(s"Poller ${flow.name} failed")
+          case Failure(err) => flow.log.error(err, s"Poller ${flow.name} failed")
         }
       }
     }
     def dispose() {
       scheduledAction.cancel()
+      dest.dispose()
     }
     val flow = f
   }
-  class PollPullEndpoint[A <: PullEndpoint](f: Flow, endpoint: EndpointFactory[A], every: Duration) extends endpoint.base.BaseSource {
+  class PollPullEndpoint[A <: PullEndpoint](f: Flow, endpoint: EndpointFactory[A], initialDelay: Duration, every: Duration) extends endpoint.base.BaseSource {
     lazy val dest = endpoint(f)
     type Payload = A#Payload
 
     var scheduledAction: akka.actor.Cancellable = _
     def start() {
+      dest.start()
       implicit val ec = flow.workerActorsExecutionContext
-      scheduledAction = appContext.actorSystem.scheduler.schedule(every, every) {
-        println(s"Flow ${flow.name} polling")
+      scheduledAction = appContext.actorSystem.scheduler.schedule(initialDelay, every) {
+        flow.log.debug(s"Flow ${flow.name} polling")
         dest.pull onComplete {
           case Success(response)  => messageArrived(response.asInstanceOf[Message[Payload]])
-          case Failure(err) => println(s"Poller ${flow.name} failed")
+          case Failure(err) => flow.log.error(err, s"Poller ${flow.name} failed")
         }
-
       }
     }
     def dispose() {
       scheduledAction.cancel()
+      dest.dispose()
     }
     val flow = f
   }
-  def Poll[A <: Askable, P](endpoint: EndpointFactory[A], every: Duration, message: Message[P])(implicit ev: TypeSupportedByTransport[A#SupportedTypes, P]) = new EndpointFactory[PollAskableEndpoint[A, P]] {
-    def apply(f: Flow) = new PollAskableEndpoint(f, endpoint, every, message)
+  def Poll[A <: Askable, P](endpoint: EndpointFactory[A], every: Duration, message: Message[P], initialDelay: Duration = Duration.Zero)(implicit ev: TypeSupportedByTransport[A#SupportedTypes, P]) = new EndpointFactory[PollAskableEndpoint[A, P]] {
+    def apply(f: Flow) = new PollAskableEndpoint(f, endpoint, initialDelay, every, message)
   }
-  def Poll[A <: PullEndpoint](endpoint: EndpointFactory[A], every: Duration) = new EndpointFactory[PollPullEndpoint[A]] {
-    def apply(f: Flow) = new PollPullEndpoint(f, endpoint, every)
+  def Poll[A <: PullEndpoint](endpoint: EndpointFactory[A], every: Duration, initialDelay: Duration = Duration.Zero) = new EndpointFactory[PollPullEndpoint[A]] {
+    def apply(f: Flow) = new PollPullEndpoint(f, endpoint, initialDelay, every)
   }
 
 }
