e7ff5f7813d2d26c3836aea7e630740cc6f38480
Rodrigo Cano <ioniviil@gmail.com>
2012-10-04 10:59:24 -0300

    Improved implicit macro that finds the logic message

--- core/src/main/scala/uy/com/netlabs/esb/MacroTest.scala
+++ core/src/main/scala/uy/com/netlabs/esb/MacroTest.scala
@@ -22,16 +22,20 @@ object MacroTest {
   
   def normalMethod = "normalMethod"
   
-  def replaceMeWithMethodImpl(c: scala.reflect.macros.Context)(): c.Expr[Unit] = {
+  def replaceMeWithMethodImpl(c: scala.reflect.macros.Context)() = {
     import c.universe._
     val normalDefTree = reify(normalMethod).tree
     println(showRaw(normalDefTree) + " - " + normalDefTree.pos)
-    val tree = DefDef(Modifiers(), newTermName("myMethod"), List(), List(), TypeTree(), Literal(Constant("hi there")))
-    val e = c.Expr(tree)
-    e
+//    val tree = DefDef(Modifiers(), newTermName("myMethod"), List(), List(), TypeTree(), Literal(Constant("hi there")))
+//    val e = c.Expr(tree)
+//    e
+    val t = c.parse("def myMethod = \"Hi there\"")
+    t.symbol = c.macroApplication.symbol
+//    for (n <- t) n.symbol = c.macroApplication.symbol
+    c.Expr(t)
   }
   
-  def replaceMeWithMethod(): Unit = macro replaceMeWithMethodImpl
+  def replaceMeWithMethod() = macro replaceMeWithMethodImpl
   
   
 //  class TransformerList[SupportedTypes] {
