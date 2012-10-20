a2a17824161e154ad7e08bfc7f3a638a723d96cf
Rodrigo Cano <ioniviil@gmail.com>
2012-10-20 01:13:59 -0200

    Abstracted many patterns over NIO, removed class Streams

--- endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/Consumer.scala
+++ endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/Consumer.scala
@@ -1,21 +1,78 @@
 package uy.com.netlabs.esb.endpoint.stream
 
 import java.nio.ByteBuffer
+import java.io.EOFException
+import java.nio.channels.ReadableByteChannel
 
 /**
  * Iteratee based concept of Consumer, though this one only feeds on chunks of bytes. No more
  * generalization needed.
  */
 trait Consumer[State, Prod] {
-  
+
   sealed trait ConsumerResult {
     def state: State
   }
   case class ByProduct(content: Seq[Prod], state: State) extends ConsumerResult
   case class NeedMore(state: State) extends ConsumerResult
-  
+
   def initialState: State
   def consume(input: Input[State]): ConsumerResult
+}
+
+object Consumer {
+
+  /**
+   * Helper class that implements the state handling of a consumer.
+   * It uses the consumer to iterate over the content, saving the
+   * produced state in between, and feeding it in each call.
+   */
+  case class Synchronous[State, Prod](consumer: Consumer[State, Prod], readBuffer: Int) {
+
+    private val inBff = ByteBuffer.allocate(readBuffer)
+    private var lastState = consumer.initialState
+    private var bufferedInput: Seq[Prod] = Seq.empty
+    def consume(chnl: ReadableByteChannel): Prod = {
+      if (bufferedInput.nonEmpty) {
+        val res = bufferedInput.head
+        bufferedInput = bufferedInput.tail
+        res
+      } else {
+        def iterate(): Prod = {
+          inBff.clear()
+          val read = chnl.read(inBff)
+          inBff.flip
+          val r = consumer.consume(Content(lastState, inBff))
+          lastState = r.state
+          r match {
+            case consumer.NeedMore(_) =>
+              if (read < 0) throw new EOFException()
+              else iterate()
+            case consumer.ByProduct(content, state) =>
+              bufferedInput = content.tail
+              content.head
+          }
+        }
+        iterate()
+      }
+    }
+  }
+
+  def Asynchronous[State, Prod](consumer: Consumer[State, Prod])(handler: Prod => Unit): ByteBuffer => Unit = {
+    var state = consumer.initialState
+    content => {
+      //      debug("Reading " + content.remaining() + " bytes")
+      val res = if (!content.hasRemaining()) consumer.consume(NoMoreInput(state))
+      else consumer.consume(Content(state, content))
+      //      debug("Reading result: " + res)
+      state = res.state
+      res match {
+        case consumer.ByProduct(ps, s) =>
+          ps foreach handler
+        case _ =>
+      }
+    }
+  }
 }
 
 sealed trait Input[State] {
---
+++ endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/SelectionKeyReactor.scala
@@ -0,0 +1,26 @@
+package uy.com.netlabs.esb
+package endpoint.stream
+
+import java.nio.channels.SelectionKey
+
+/**
+ * Attached to a SelectionKey, this class allows registering for
+ * the valid actions of a SelectionKey
+ */
+class SelectionKeyReactor {
+  @volatile var beforeProcessingInterests: SelectionKey => Unit = _
+  @volatile var reader: SelectionKey => Unit = _
+  @volatile var writer: SelectionKey => Unit = _
+  @volatile var connector: SelectionKey => Unit = _
+  @volatile var acceptor: SelectionKey => Unit = _
+  @volatile var afterProcessingInterests: SelectionKey => Unit = _
+  
+  def react(key: SelectionKey) {
+    if (beforeProcessingInterests != null) beforeProcessingInterests(key)
+    if (key.isReadable() && reader != null) reader(key)
+    if (key.isWritable() && writer != null) writer(key)
+    if (key.isConnectable() && connector != null) connector(key)
+    if (key.isAcceptable() && acceptor!= null) acceptor(key)
+    if (beforeProcessingInterests != null) beforeProcessingInterests(key)
+  }
+}
