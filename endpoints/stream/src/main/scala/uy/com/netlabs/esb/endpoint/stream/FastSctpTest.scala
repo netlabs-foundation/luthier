1942f629b7d03b4ab692a1e2ef4cd0ac0d7a56f4
Rodrigo Cano <ioniviil@gmail.com>
2012-10-25 14:12:05 -0200

    Added sctp

---
+++ endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/FastSctpTest.scala
@@ -0,0 +1,31 @@
+package uy.com.netlabs.esb
+package endpoint
+package stream
+
+import Sctp._
+
+object FastSctpTest extends App {
+  val test = new Flows {
+    val appContext = AppContext.quick("streams")
+    new Flow("ss")(Server(1500, 1024)) {
+      logic { client =>
+        new Flow("clientHandler-" + client.payload)(Handler(client, 0, consumers.lines(), serializers.string)) {
+          logic { m: Message[String] =>
+            println("Client message: " + m.payload)
+            m map (0 -> _)
+          }
+        }
+        new Flow("clientHandler2-" + client.payload)(Handler(client, 1, consumers.lines())) {
+          logic { m: Message[String] =>
+            m.payload match {
+              case exit if exit.trim == "exit" =>
+                println("Shutting down client")
+                closeClient(client)
+              case other =>
+            }
+          }
+        }
+      }
+    }.start
+  }
+}
