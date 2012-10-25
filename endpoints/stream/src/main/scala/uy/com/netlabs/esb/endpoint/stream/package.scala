78ecd4b99a5d5cae76549af161116109f7d3c7ca
rcano <rcano@netlabs.com.uy>
2012-10-25 21:02:21 -0200

    Fixed Sctp
    There were two bugs, one in the handling of the AssociationChangeNotification, where the client
    was beign disposed under a correct association setup, and the other when writing content, for example
    to a wrong stream, it would fail but not remove the pending task, so it would enter an endless loop
    trying to send it wrongly.

--- endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/package.scala
+++ endpoints/stream/src/main/scala/uy/com/netlabs/esb/endpoint/stream/package.scala
@@ -5,7 +5,7 @@ import java.nio.channels.SelectionKey
 object `package` {
   @inline
   private[stream] final def debug(s: =>Any) {
-//    println(s)
+    println(s)
   }
   
   private[stream] def keyDescr(k: SelectionKey) = s"Key($k),R:${k.isReadable()},W:${k.isWritable()},A:${k.isAcceptable()}. Chnl: ${k.channel()}"
