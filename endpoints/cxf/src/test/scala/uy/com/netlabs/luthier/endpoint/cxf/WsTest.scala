cad2a2e2e6619d6e08b6cacbbfd1b9d37269f061
rcano <rcano@netlabs.com.uy>
2013-03-01 19:18:01 -0200

    Added license

--- endpoints/cxf/src/test/scala/uy/com/netlabs/luthier/endpoint/cxf/WsTest.scala
+++ endpoints/cxf/src/test/scala/uy/com/netlabs/luthier/endpoint/cxf/WsTest.scala
@@ -1,3 +1,33 @@
+/**
+ * Copyright (c) 2013, Netlabs S.R.L. <contacto@netlabs.com.uy>
+ * All rights reserved.
+ *
+ * This software is dual licensed as GPLv2: http://gnu.org/licenses/gpl-2.0.html,
+ * and as the following 3-clause BSD license. In other words you must comply to
+ * either of them to enjoy the permissions they grant over this software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name "netlabs" nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NETLABS S.R.L.  BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package uy.com.netlabs.luthier
 package endpoint
 package cxf
@@ -16,7 +46,7 @@ class WsTest extends BaseFlowsTest {
     it("Should be able to publish a web service, and also consult it") {
       new Flows {
         val appContext = testApp
-        
+
         @WebService
         trait WsDef {
           def echo(msg: String): String
@@ -26,9 +56,9 @@ class WsTest extends BaseFlowsTest {
           logic {m => m} //really simply echo!
         }
         server.start
-        
+
         val timeout = 10.seconds
-        val res = inFlow { (flow, m) => 
+        val res = inFlow { (flow, m) =>
           import flow._
           val greet = "Hi there!"
           WsInvoker[String](WsClient(url + "/testws?wsdl"), "echo", true).ask(m.map(_ => Seq(greet)))
@@ -45,7 +75,7 @@ class WsTest extends BaseFlowsTest {
     it("Should be able to handle dynamic types") {
       new Flows {
         val appContext = testApp
-        
+
         @WebService
         trait WsDef {
           def echo(msg: EchoMessage): String
@@ -55,13 +85,13 @@ class WsTest extends BaseFlowsTest {
           logic {m => m.map(_.content)} //really simply echo!
         }
         server.start
-        
+
         val timeout = 10.seconds
         val client = WsClient(url + "/anotherws?wsdl")
-        val res = inFlow { (flow, m) => 
+        val res = inFlow { (flow, m) =>
           import flow._
           val greet = "Hi there!"
-            
+
           val em = client.instance("uy.com.netlabs.luthier.endpoint.cxf.EchoMessage")
           em.setContent(greet)
           val resp = Await.result(WsInvoker[String](client, "echo", true).ask(m.map(_ => Seq(em.instance))), timeout)
@@ -73,7 +103,7 @@ class WsTest extends BaseFlowsTest {
       }
     }
   }
-   
+
 }
 class EchoMessage(var content: String) {
   def this() = this("")
