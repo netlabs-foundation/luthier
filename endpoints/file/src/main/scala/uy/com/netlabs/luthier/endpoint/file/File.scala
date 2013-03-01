9492771313197687e8a5c9007e42c2a383f5ea48
rcano <rcano@netlabs.com.uy>
2013-03-01 19:18:17 -0200

    Added license

--- endpoints/file/src/main/scala/uy/com/netlabs/luthier/endpoint/file/File.scala
+++ endpoints/file/src/main/scala/uy/com/netlabs/luthier/endpoint/file/File.scala
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2013, Netlabs S.R.L. <contacto@netlabs.com.uy>
+ * All rights reserved.
+ *
+ * This software is dual licensed as GPLv2: http://gnu.org/licenses/gpl-2.0.html,
+ * and as the following 3-clause BSD license. In other words you must comply to
+ * either of them to enjoy the permissions they grant over this software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name "netlabs" nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NETLABS S.R.L.  BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package uy.com.netlabs.luthier
 package endpoint.file
 
@@ -7,26 +38,27 @@ import java.nio.file._
 import scala.concurrent.ExecutionContext
 
 object File {
-  private case class EF(path: String, charset: String, ioThreads: Int) extends EndpointFactory[FileEndpoint] {
+  private case class EF(path: Path, charset: String, ioThreads: Int) extends EndpointFactory[FileEndpoint] {
     def apply(f: Flow) = new FileEndpoint(f, path, charset, ioThreads)
   }
-  def apply(path: String, charset: String = "UTF-8", ioThreads: Int = 1): EndpointFactory[FileEndpoint] = EF(path, charset, ioThreads)
+  def apply(path: String, charset: String = "UTF-8",
+            ioThreads: Int = 1)(implicit appContext: AppContext): EndpointFactory[FileEndpoint] = EF(appContext.rootLocation.resolve(path), charset, ioThreads)
 }
-class FileEndpoint(f: Flow, path: String, charset: String, ioThreads: Int) extends endpoint.base.BaseSink with endpoint.base.BasePullEndpoint {
+class FileEndpoint(f: Flow, path: Path, charset: String, ioThreads: Int) extends endpoint.base.BaseSink with endpoint.base.BasePullEndpoint {
   type Payload = Array[Byte]
   type SupportedTypes = Iterable[Byte] :: Array[Byte] :: String :: java.io.Serializable :: TypeNil
 
   implicit val flow = f
-  
+
   val ioProfile = endpoint.base.IoProfile.threadPool(ioThreads)
-  
+
   protected def pushMessage[Payload: SupportedType](msg: Message[Payload]) {
     msg.payload match {
-      case it: Iterable[Byte @unchecked] => Files.write(Paths.get(path), it.toArray, StandardOpenOption.CREATE)
-      case arr: Array[Byte] => Files.write(Paths.get(path), arr, StandardOpenOption.CREATE)
-      case str: String => Files.write(Paths.get(path), str.getBytes(charset), StandardOpenOption.CREATE)
+      case it: Iterable[Byte @unchecked] => Files.write(path, it.toArray, StandardOpenOption.CREATE)
+      case arr: Array[Byte] => Files.write(path, arr, StandardOpenOption.CREATE)
+      case str: String => Files.write(path, str.getBytes(charset), StandardOpenOption.CREATE)
       case obj: java.io.Serializable =>
-        val out = new java.io.ObjectOutputStream(Files.newOutputStream(Paths.get(path), StandardOpenOption.CREATE))
+        val out = new java.io.ObjectOutputStream(Files.newOutputStream(path, StandardOpenOption.CREATE))
         try out.writeObject(obj)
         finally {
           if (out != null) out.close
@@ -35,7 +67,7 @@ class FileEndpoint(f: Flow, path: String, charset: String, ioThreads: Int) exten
     }
   }
 
-  protected def retrieveMessage(mf) = mf(Files.readAllBytes(Paths.get(path)))
+  protected def retrieveMessage(mf) = mf(Files.readAllBytes(path))
 
   def dispose() {
     ioProfile.dispose()
