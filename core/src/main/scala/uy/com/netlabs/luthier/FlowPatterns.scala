5cf2081eaf5751b273d9dffce1084ec9a4dec61b
rcano <rcano@netlabs.com.uy>
2013-03-01 19:16:56 -0200

    Added license

--- core/src/main/scala/uy/com/netlabs/luthier/FlowPatterns.scala
+++ core/src/main/scala/uy/com/netlabs/luthier/FlowPatterns.scala
@@ -1,3 +1,33 @@
+/**
+ * Copyright (c) 2013, Netlabs S.R.L. <contacto@netlabs.com.uy>
+ * All rights reserved.
+ *
+ * This software is dual licensed as GPLv2: http://gnu.org/licenses/gpl-2.0.html,
+ * and as the following 3-clause BSD license. In other words you must comply to
+ * either of them to enjoy the permissions they grant over this software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name "netlabs" nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NETLABS S.R.L.  BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package uy.com.netlabs.luthier
 
 import scala.util._
@@ -27,7 +57,7 @@ trait FlowPatterns {
     import fc.flow.workerActorsExecutionContext
     val promise = Promise[T]()
     op onComplete { t =>
-      val failed = isFailure(t) 
+      val failed = isFailure(t)
       if (failed && maxAttempts > 1) {
         if (fc.flow.log.isEnabled(logRetriesWithLevel)) fc.flow.log.log(logRetriesWithLevel, s"""Attempt for "$description" failed. Remaining $maxAttempts attempts. Retrying...""")
         promise.completeWith(retryAttempts(maxAttempts - 1, description, logRetriesWithLevel)(op)(isFailure))
@@ -61,7 +91,7 @@ trait FlowPatterns {
           }
         } else {
           if (failed && fc.flow.log.isEnabled(logRetriesWithLevel)) fc.flow.log.log(logRetriesWithLevel, s"""Accumulated backoff($acc) ≥ maxBackoff($maxBackoff). "$description" failed.""")
-          promise.complete(t) 
+          promise.complete(t)
         }
       }
       promise.future
--- core/src/main/scala/uy/com/netlabs/luthier/FlowRun.scala
+++ core/src/main/scala/uy/com/netlabs/luthier/FlowRun.scala
@@ -1,3 +1,33 @@
+/**
+ * Copyright (c) 2013, Netlabs S.R.L. <contacto@netlabs.com.uy>
+ * All rights reserved.
+ *
+ * This software is dual licensed as GPLv2: http://gnu.org/licenses/gpl-2.0.html,
+ * and as the following 3-clause BSD license. In other words you must comply to
+ * either of them to enjoy the permissions they grant over this software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name "netlabs" nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NETLABS S.R.L.  BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package uy.com.netlabs.luthier
 
 import scala.language._
@@ -8,7 +38,7 @@ import scala.collection.mutable._
  * Useful to store run temporal information as well as a message factory.
  */
 trait FlowRun[+FlowType <: Flow] extends MessageFactory {
-  
+
   /**
    * Message that started the run, i.e.: the message declared in logic
    */
@@ -21,7 +51,7 @@ trait FlowRun[+FlowType <: Flow] extends MessageFactory {
    * A run context to put anything you like.
    */
   val context: Map[Any, Any] = scala.collection.concurrent.TrieMap.empty
-  
+
   private[this] var doneReactions = Set.empty[() => Unit]
   def afterFlowRun(code: => Unit) {
     doneReactions += (() => code)
@@ -29,7 +59,7 @@ trait FlowRun[+FlowType <: Flow] extends MessageFactory {
   private[luthier] def flowRunCompleted() {
     for (r <- doneReactions) r()
   }
-  
+
   private[this] var lastProducedMessage0: Message[_] = _
   private[this] var lastReceivedMessage0: Message[_] = rootMessage
   def lastProducedMessage: Message[_] = lastProducedMessage0
@@ -43,7 +73,7 @@ trait FlowRun[+FlowType <: Flow] extends MessageFactory {
     lastProducedMessage0 = res //FIXME: not sure this makes much sense...
     res
   }
-  
+
   def apply[P](payload: P) = {
     val res = lastReceivedMessage0.map(_ => payload)
     lastProducedMessage0 = res
